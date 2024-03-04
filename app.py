7058d15349582efc47fda444116900bbc7182acf
Thomas Kim <tkimm156@gmail.com>
2024-03-04 13:37:45 -0600

    create api get request for all cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
 """Flask app for Cupcakes"""
+import os
+
+from flask import Flask, jsonify
+
+from models import connect_db, Cupcake, db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+
+connect_db(app)
+
+@app.get("/api/cupcakes")
+def get_all_cupcakes_data():
+
+    cupcakes = Cupcake.query.all()
+
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes = serialized)
+
--- models.py
+++ models.py
@@ -1,1 +1,58 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """blueprint for making cupcake"""
+
+    __tablename__ = "cupcakes"
+
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default="https://tinyurl.com/demo-cupcake"
+    )
+
+    def serialize(self):
+        """Serialize cupcake to dictionary"""
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image_url": self.image_url
+        }
